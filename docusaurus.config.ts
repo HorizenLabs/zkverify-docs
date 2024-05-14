3432887d02b89836ab21e163311edfc8d6577cc7
la10736 <michele.damico@gmail.com>
2024-05-14 11:04:33 +0200

    Verification pallets docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -71,6 +71,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['rust'],
     },
   } satisfies Preset.ThemeConfig,
 };
