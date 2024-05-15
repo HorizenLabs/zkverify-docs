b3e57939abc886af415728a4bee17f57e873fa0e
Michele d'Amico <michele.damico@gmail.com>
2024-05-15 15:18:40 +0200

    Verification pallets docs (#7)
    * Verification pallets docs
    
    * Update docs/08-verification_pallets.md
    
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
    
    * Update docs/08-verification_pallets.md
    
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
    
    * Update docs/08-verification_pallets.md
    
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
    
    * Update docs/08-verification_pallets.md
    
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
    
    * Update docs/08-verification_pallets.md
    
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>
    
    * Apply suggestions
    
    * Add solidity highlighting support
    
    * Mark code snipped as solidity
    
    * Update on chain logic
    
    ---------
    
    Co-authored-by: johncamardo <85902365+johncamardo@users.noreply.github.com>
    Co-authored-by: DanieleDiBenedetto <50587279+DanieleDiBenedetto@users.noreply.github.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -70,6 +70,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['solidity'],
     },
   } satisfies Preset.ThemeConfig,
 };
