589807f4c77c5d226736afa86210475ebdab6d72
Yuriko Inaba <yurikoainaba@gmail.com>
2024-09-10 09:49:06 -0400

    fix

--- scripts/github_readme.js
+++ scripts/github_readme.js
@@ -13,27 +13,27 @@ const remoteReadmes = [
 const fetchMarkdown = async (githubPath, callback) => {
     try {        
         // Fetch markdown
-        const remotePath = `https://api.github.com/repos/${githubPath}/contents/README.md`
-        const response = await axios.get(
-            remotePath,
-            {
-                headers: {
-                    'Accept': 'application/vnd.github.v3.raw',
-                },
-            }
-        );
+        // const remotePath = `https://api.github.com/repos/${githubPath}/contents/README.md`
+        // const response = await axios.get(
+        //     remotePath,
+        //     {
+        //         headers: {
+        //             'Accept': 'application/vnd.github.v3.raw',
+        //         },
+        //     }
+        // );
 
-        let text = response.data;
+        // let text = response.data;
 
-        // const response = fs.readFileSync(path.join(__dirname, '..', `/static/output/${githubPath}-og.md`), 'utf-8');
-        // let text = response;
+        const response = fs.readFileSync(path.join(__dirname, `../static/output/${githubPath}-og.md`), 'utf-8');
+        let text = response;
 
         if (callback) {
           text = await callback(text);
         }
 
         // Define the path to the new file
-        const filePath = path.join(__dirname, '..', `/static/output/${githubPath}.md`);
+        const filePath = path.join(__dirname, `../static/output/${githubPath}.md`);
 
         // Write data to the new file
         fs.writeFile(filePath, text, 'utf8', (err) => {
